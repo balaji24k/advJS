e1db9f4f3dc20625dd26d208a83cca7af7a0f381
balaji24k <balaji325s@gmail.com>
2022-11-19 15:21:06 +0530

    second

---
+++ task3/dom.js
@@ -0,0 +1,16 @@
+console.log(document.URL);
+console.log(document.title);
+
+document.title="myList";
+
+console.log(document.all[10]);
+
+var headerTitle = document.getElementById('header-title');
+headerTitle.style.color="blue"
+
+headerTitle.style.borderBottom="solid 3px #000"
+
+
+var item=document.getElementsByClassName('title')
+item[0].style.color='green';
+item[0].style.fontWeight='bold'
