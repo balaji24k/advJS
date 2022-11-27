72360119c1c3b7db228227a01a5c82365aab9edf
balaji24k <balaji325s@gmail.com>
2022-11-27 18:24:32 +0530

    Task 13 added

--- Form/main.js
+++ Form/main.js
@@ -12,8 +12,39 @@ function saveToLocalStorage(event) {
 }
 
 function showNewUserOnScreen (user) {
+
+    document.getElementById('email').value='';
+    document.getElementById('name').value='';
+
+    
     const parentNode = document.getElementById('listOfUsers');
-    const childHTML = `<li> ${user.name} - ${user.email} </li>`
+    const childHTML = `<li id=${user.email}> ${user.name} - ${user.email}
+        <button onclick=deleteUser('${user.email}')> Delete User </button> 
+        <button onclick=editUser('${user.email}','${user.name}')> Edit User </button>
+        </li>`
 
     parentNode.innerHTML = parentNode.innerHTML+childHTML;
 }
+
+function deleteUser(emailId){
+    console.log(emailId)
+    localStorage.removeItem(emailId);
+    removeUserFromScreen(emailId);
+
+}   
+
+function removeUserFromScreen(emailId){
+    const parentNode = document.getElementById('listOfUsers');
+    const childNodeToBeDeleted = document.getElementById(emailId);
+
+    parentNode.removeChild(childNodeToBeDeleted)
+}
+
+function editUser(emailId, name) {
+    document.getElementById('email').value=emailId;
+    document.getElementById('name').value=name;
+
+    deleteUser(emailId)
+
+}
+    
