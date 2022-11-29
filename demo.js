9a56afca52d5106df16d9d2f2da6ae8a70ab0c08
balaji24k <balaji325s@gmail.com>
2022-11-29 12:30:58 +0530

    RegistrationForm Added

--- demo.js
+++ demo.js
@@ -1,1 +1,18 @@
-console.log("Monday")
+class count_objects 
+{
+  static i= 0;
+constructor()
+{
+  count_objects.i=count_objects.i+1;
+}
+}
+   var a= new count_objects();
+   var b = new count_objects();
+   var c = new count_objects();
+   var d= new count_objects();
+   var e = new count_objects();
+   
+
+
+
+  console.log(count_objects.i);
